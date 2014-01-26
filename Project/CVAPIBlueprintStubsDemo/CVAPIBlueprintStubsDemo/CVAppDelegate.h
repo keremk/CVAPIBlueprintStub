ecdedb205a308833ade4a9c3011202e0087c0780
Kerem Karatal <kkaratal@yahoo.com>
2014-01-26 14:32:04 -0800

    Adds the demo xcode project file.

---
+++ Project/CVAPIBlueprintStubsDemo/CVAPIBlueprintStubsDemo/CVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CVAppDelegate.h
+//  CVAPIBlueprintStubsDemo
+//
+//  Created by Kerem Karatal on 1/26/14.
+//  Copyright (c) 2014 CodingVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project/CVAPIBlueprintStubsDemo/CVAPIBlueprintStubsDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CVAPIBlueprintStubsDemo
+//
+//  Created by Kerem Karatal on 1/26/14.
+//  Copyright (c) 2014 CodingVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CVAppDelegate class]));
+  }
+}
